a8eef8788212bc2592b5eda96c50f345c1c3bd81
AporSzakacs <apor.szakacs@stud.ubbcluj.ro>
2026-03-25 13:57:38 +0200

    forgotten library idk

---
+++ LAB5/CODEFROMPROF/Labor3_10_RGB_IR_remote/Labor3_10_RGB_IR_remote.ino
@@ -0,0 +1,128 @@
+
+#include <Arduino.h>
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp>
+
+#include <FastLED.h>
+
+#define LED_PIN     5
+#define NUM_LEDS    64
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+byte led_matrix[8][8];
+
+#define UPDATES_PER_SECOND 100
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+#include <Arduino.h>
+//Define macros for input and output pin etc.
+#include "PinDefinitionsAndMore.h"
+#include <IRremote.hpp>
+
+void setup()
+{
+  // power-up safety delay
+  delay(3000); 
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(  BRIGHTNESS );
+    
+  currentPalette = myRedWhiteBluePalette_p;
+  currentBlending = LINEARBLEND;
+  
+  //set up the Serial port
+  Serial.begin(9600);
+
+  IrReceiver.begin(3, ENABLE_LED_FEEDBACK, USE_DEFAULT_FEEDBACK_LED_PIN);
+  Serial.print(F("Ready to receive Color Buttons  "));
+  Serial.print(F("at pin "));
+  Serial.println("3");
+}
+ 
+void loop()
+{
+   
+if (IrReceiver.decode()) 
+  {
+
+        IrReceiver.resume(); // Enable receiving of the next value
+
+        if (IrReceiver.decodedIRData.command == 0x37) 
+          {
+            Serial.println("RED");
+            FillLEDs_in_matrix(0);
+            FastLED.show();
+            //FastLED.delay(1000 / UPDATES_PER_SECOND);
+          } 
+        else if (IrReceiver.decodedIRData.command == 0x36) 
+          {
+            Serial.println("GREEN");
+            FillLEDs_in_matrix(1);
+            FastLED.show();
+            //FastLED.delay(1000 / UPDATES_PER_SECOND);
+          }
+        else if (IrReceiver.decodedIRData.command == 0x32) 
+          {
+            Serial.println("YELLOW");
+            FillLEDs_in_matrix(2);
+            FastLED.show();
+            //FastLED.delay(1000 / UPDATES_PER_SECOND);
+          }
+        else if (IrReceiver.decodedIRData.command == 0x34) 
+          {
+            Serial.println("BLUE");
+            FillLEDs_in_matrix(3);
+            FastLED.show();
+            //FastLED.delay(1000 / UPDATES_PER_SECOND);
+          }
+
+
+  }
+  
+    
+  //FillLEDs_in_matrix();
+    
+  //FastLED.show();
+  //FastLED.delay(1000 / UPDATES_PER_SECOND);
+
+}
+ 
+void FillLEDs_in_matrix(byte color)
+{
+    byte i_led;
+
+    for(i_led=0;i_led<NUM_LEDS;i_led++)
+      leds[i_led] = ColorFromPalette(  myRedWhiteBluePalette_p, 2, 255, currentBlending);
+
+  delay(250);
+
+}
+
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
+{
+    CRGB::Red,
+    CRGB::Green,
+    CRGB::Yellow,
+    CRGB::Blue,
+    
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Black,
+    
+    CRGB::Red,
+    CRGB::Red,
+    CRGB::Gray,
+    CRGB::Gray,
+    CRGB::Blue,
+    CRGB::Blue,
+    CRGB::Black,
+    CRGB::Black
+};
---
+++ LAB5/SNAKE/SNAKE.ino
@@ -0,0 +1,120 @@
+#include <FastLED.h>
+
+#define LED_PIN 5
+#define NUM_LEDS 64
+#define BRIGHTNESS 64
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+byte snake_length = 15;
+byte snake[15]; 
+
+byte led_matrix[8][8];
+
+#define UPDATES_PER_SECOND 100
+
+CRGBPalette16 currentPalette;
+TBlendType currentBlending;
+
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+int analog_potmeter_value_x;
+int analog_potmeter_value_y;
+int scaled_value_x;
+int scaled_value_y;
+
+void setup() {
+  // power-up safety delay
+  delay(3000);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  currentPalette = myRedWhiteBluePalette_p;
+  currentBlending = LINEARBLEND;
+
+  //set up the Serial port
+  Serial.begin(9600);
+
+  for (byte i = 0; i < snake_length; i++) {
+    snake[i] = 32;
+  }
+}
+
+void loop() {
+
+  //read the potentiometer value
+  analog_potmeter_value_x = analogRead(A0);
+  analog_potmeter_value_y = analogRead(A1);
+
+  Serial.print("Potentiometer values = ");
+  Serial.print(analog_potmeter_value_x);
+  Serial.print(",");
+  Serial.print(analog_potmeter_value_y);
+
+  //this is a value between 0 ... 1023
+  //scale it to 0..7
+  scaled_value_x = (int)round((analog_potmeter_value_x * 7.0 / 1023.0));
+  scaled_value_y = (int)round((analog_potmeter_value_y * 7.0 / 1023.0));
+
+  for (byte i = snake_length - 1; i >= 1; i--) {
+    snake[i] = snake[i-1];
+  }
+  snake[0] = scaled_value_x * 8 + scaled_value_y;
+
+  Serial.print(", Scaled values =  ");
+  Serial.print(scaled_value_x);
+  Serial.print(",");
+  Serial.println(scaled_value_y);
+
+  FillLEDs_in_matrix();
+
+  FastLED.show();
+  FastLED.delay(1000 / UPDATES_PER_SECOND);
+
+  delay(100);
+}
+
+void FillLEDs_in_matrix() {
+  byte i, j;
+  byte i_led;
+
+  for (i = 0; i < 8; i++)
+    for (j = 0; j < 8; j++) {
+      led_matrix[i][j] = 0;
+    }
+
+  led_matrix[scaled_value_x][scaled_value_y] = 1;
+
+  for (i = 0; i < 8; i++)
+    for (j = 0; j < 8; j++) {
+      i_led = i * 8 + j;
+      leds[i_led] = ColorFromPalette(myRedWhiteBluePalette_p, 1, 255 * led_matrix[i][j], currentBlending);
+    }
+
+  for (byte i = 0; i < snake_length; i++) {
+    leds[snake[i]] = ColorFromPalette(myRedWhiteBluePalette_p, 1, 255, currentBlending);
+  }
+}
+
+const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM = {
+  CRGB::Red,
+  CRGB::Gray,  // 'white' is too bright compared to red and blue
+  CRGB::Blue,
+  CRGB::Black,
+
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Black,
+
+  CRGB::Red,
+  CRGB::Red,
+  CRGB::Gray,
+  CRGB::Gray,
+  CRGB::Blue,
+  CRGB::Blue,
+  CRGB::Black,
+  CRGB::Black
+};
